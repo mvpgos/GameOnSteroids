1efd1af5155eeafa4e99983d622ef8240056d157
gamsteron <gamsteron@gmail.com>
2016-05-10 09:07:55 +0200

    Update Ahri.lua

--- Ahri.lua
+++ Ahri.lua
@@ -2,7 +2,7 @@ if myHero.charName ~= "Ahri" then
   return 
 end
 
-local ver = "1.0"
+local ver = "1.1"
 function AutoUpdate(data)
     if tonumber(data) > tonumber(ver) then
         PrintChat("New version found! " .. data)
@@ -17,7 +17,7 @@ GetWebResultAsync("https://raw.githubusercontent.com/gamsteron/GameOnSteroids/ma
 local UtilsManager = {}
 local ImmobileBuffs  = {}
 local WaypointManager = {}
-local AllyTeam = GetTeam(myHero)
+local AllyTeam = myHero.team
 local aa_wind, aa_anim, move_next = 0, 0, 0
 local focus_target = nil
 
@@ -29,7 +29,7 @@ menu:ColorPick("c", "Color", {255,255,0,0})
 OnIssueOrder(function(order)
         if order.flag == 3 then
                 local t = GetTickCount()
-                local s = GetAttackSpeed(myHero) * GetBaseAttackSpeed(myHero)
+                local s = myHero.attackSpeed * myHero.baseAttackSpeed
                 aa_wind = t+( 200 / s )
                 aa_anim = t+( 1000 / s )
         end
@@ -40,16 +40,16 @@ end)
 
 OnLoad(function()
         for i, enemy in ipairs(GetEnemyHeroes()) do
-                local name = GetObjectName(enemy)
+                local name = enemy.charName
                 menu.f:Boolean(name, name, true)
-                WaypointManager[GetNetworkID(enemy)] = { dir = {}, time = 0, from = {}, to = {}, dist = 0 }
-                UtilsManager[GetNetworkID(enemy)] = { CanMove = true, IsMoving = false, LastStopMoveTime = 0, IsAttacking = false, AALast = 0, AACastDelay = 0, IsImmobile = false, Immobile = {} }
+                WaypointManager[enemy.networkID] = { dir = {}, time = 0, from = {}, to = {}, dist = 0 }
+                UtilsManager[enemy.networkID] = { CanMove = true, IsMoving = false, LastStopMoveTime = 0, IsAttacking = false, AALast = 0, AACastDelay = 0, IsImmobile = false, Immobile = {} }
         end
         ImmobileBuffs = { [GetBuffTypeList().Stun] = true, [GetBuffTypeList().Taunt] = true, [GetBuffTypeList().Snare] = true, [GetBuffTypeList().Fear] = true, [GetBuffTypeList().Charm] = true, [GetBuffTypeList().Suppression] = true, [GetBuffTypeList().Flee] = true, [GetBuffTypeList().Knockup] = true, [GetBuffTypeList().Knockback] = true }
 end)
 
 OnProcessWaypoint(function(unit,waypoint)
-        local id = GetNetworkID(unit)
+        local id = unit.networkID
         if WaypointManager[id] then
                 if waypoint.index == 2 then
                         WaypointManager[id].from = waypoint.position
@@ -77,7 +77,7 @@ OnProcessWaypoint(function(unit,waypoint)
 end)
 
 OnProcessSpellAttack(function(unit, aa)
-        local id = GetNetworkID(unit)
+        local id = unit.networkID
         if UtilsManager[id] then
                 UtilsManager[id].AACastDelay = aa.windUpTime*1000
                 UtilsManager[id].AALast = GetTickCount()
@@ -86,7 +86,7 @@ OnProcessSpellAttack(function(unit, aa)
 end)
 
 OnUpdateBuff(function(unit, buff)
-        local id = GetNetworkID(unit)
+        local id = unit.networkID
         if WaypointManager[id] then
                 local type = buff.Type
                 if ImmobileBuffs[type] then
@@ -104,7 +104,7 @@ OnDraw(function(myHero)
 end)
 
 OnRemoveBuff(function(unit, buff)
-        local id = GetNetworkID(unit)
+        local id = unit.networkID
         local et = buff.ExpireTime
         if UtilsManager[id] then
                 for i, b in ipairs(UtilsManager[id].Immobile) do
@@ -120,7 +120,7 @@ function Orb()
         if GetTickCount() > aa_wind + GetLatency() then
                 MoveToXYZ(GetMousePos())
         end
-        local aat = GetTarget(GetRange(myHero) + GetHitBox(myHero), true)
+        local aat = GetTarget(myHero.range + myHero.boundingRadius, true)
         if aat ~= nil then
                 if GetTickCount() > aa_anim + GetLatency() then
                         AttackUnit(aat)
@@ -131,9 +131,10 @@ end
 function QLOGIC()
         if Ready(_Q) then
                 local qt = GetTarget(975, false)
+                local qtpos = et.pos
                 if qt ~= nil then
                         local i = InterceptionPoint(qt,1500,0,0.25,880)
-                        if i ~= nil and math.sqrt((i.x-qt.pos.x)*(i.x-qt.pos.x)+(i.z-qt.pos.z)* (i.z-qt.pos.z)) < 200  then
+                        if i ~= nil and math.sqrt((i.x-qtpos.x)*(i.x-qtpos.x)+(i.z-qtpos.z)* (i.z-qtpos.z)) < 300  then
                                 CastSkillShot(_Q, i)
                         else
                                 MoveToXYZ(GetMousePos())
@@ -158,9 +159,10 @@ end
 function ELOGIC()
         if Ready(_E) then
                 local et = GetTarget(975, false)
+                local etpos = et.pos
                 if et ~= nil then
                         local i = InterceptionPoint(et,1500,60,0.25,975)
-                        if i ~= nil and NoCollision(et, myHero.pos, et.pos, 100, 975) and math.sqrt((i.x-et.pos.x)*(i.x-et.pos.x)+(i.z-et.pos.z)* (i.z-et.pos.z)) < 200 then
+                        if i ~= nil and NoCollision(et, myHero.pos, etpos, 100, 975) and math.sqrt((i.x-etpos.x)*(i.x-etpos.x)+(i.z-etpos.z)* (i.z-etpos.z)) < 300 then
                                 CastSkillShot(_E, i)
                         end
                 end
@@ -179,10 +181,10 @@ function GetTarget(r, aa)
         local t = nil
         num = 10000
         for i, enemy in pairs(GetEnemyHeroes()) do
-                local name = GetObjectName(enemy)
+                local name = enemy.charName
                 if ValidTarget(enemy, r) and menu.f[name]:Value() then
-                        if GetCurrentHP(enemy) < num then
-                                num = GetCurrentHP(enemy)
+                        if enemy.health < num then
+                                num = enemy.health
                                 t = enemy
                         end
                 end
@@ -192,7 +194,7 @@ function GetTarget(r, aa)
         end
         if aa then
                 if  t ~= nil then
-                        r = r + GetHitBox(t)
+                        r = r + t.boundingRadius
                 end
         end
         return t
@@ -206,7 +208,7 @@ function NoCollision(x, a, b, h, r)
                 end
         end
         for i, enemy in pairs(GetEnemyHeroes()) do
-                if  GetNetworkID(enemy) ~= GetNetworkID(x) and ValidTarget(minion, r+150) and Collision(a, b, enemy, h) then
+                if enemy.networkID ~= x.networkID and ValidTarget(minion, r+150) and Collision(a, b, enemy, h) then
                         no_col = false
                 end
         end
@@ -214,14 +216,13 @@ function NoCollision(x, a, b, h, r)
 end
 		
 function Collision(a, b, c, h)
-        local n1 = (a.x - b.x) * (a.x - b.x) + (a.z - b.z) * (a.z - b.z)
-        local n2 = (a.x - c.x) * (a.x - c.x) + (a.z - c.z) * (a.z - c.z)
-        local ac = math.sqrt(n2)
-        local n3 = (c.x - b.x) * (c.x - b.x) + (c.z - b.z) * (c.z - b.z)
-        local a1 = math.acos((n1 + n2 - n3) / math.sqrt(4 * n1 * n2))
-        local a2 = math.acos((n1 + n3 - n2) / math.sqrt(4 * n1 * n3))
-        if a1 < math.pi / 2 and a2 < 1.75 then
-                local x = math.sin(a1) * math.sqrt(n2)
+        local n1 = math.sqrt((a.x - b.x) * (a.x - b.x) + (a.z - b.z) * (a.z - b.z))
+        local n2 = math.sqrt((a.x - c.x) * (a.x - c.x) + (a.z - c.z) * (a.z - c.z))
+        local n3 = math.sqrt((c.x - b.x) * (c.x - b.x) + (c.z - b.z) * (c.z - b.z))
+        local a_angle = math.acos((n1*n1 + n2*n2 - n3*n3) / (2 * n1 * n2))
+        local b_angle = math.acos((n1*n1 + n3*n3 - n2*n2) / (2 * n1 * n3))
+        if a_angle < math.pi / 2 and b_angle < 1.75 then
+                local x = math.sin(a_angle) * math.sqrt(n2)
                 if x < h then
                         return true
                 else
@@ -237,7 +238,9 @@ OnWndMsg(function(msg, key)
                 local obj = nil
                 for i, enemy in ipairs(GetEnemyHeroes()) do
                         if ValidTarget(enemy, 2500) then
-                                local dist = math.sqrt((enemy.pos.x - GetMousePos().x)*(enemy.pos.x - GetMousePos().x)+(enemy.pos.z- GetMousePos().z)*(enemy.pos.z - GetMousePos().z))
+                        	local mp = GetMousePos()
+                        	local ep = enemy.pos
+                                local dist = math.sqrt((ep.x - mp).x)*(ep.x - mp.x)+(ep.z- mp.z)*(ep.z - mp.z))
                                 if dist < 150 then
                                         obj = enemy
                                         break
@@ -249,7 +252,10 @@ OnWndMsg(function(msg, key)
 end)
 
 function InterceptionPoint(t,s,w,d,r)
-        local id = GetNetworkID(t)
+        local id = t.networkID
+	local mepos = myHero.pos
+	local tpos = t.pos
+	local distmt = math.sqrt((mepos.x - tpos.x)*(mepos.x - tpos.x)+(mepos.z - tpos.z)*(mepos.z - tpos.z))
         if UtilsManager[id].IsImmobile then
                 local max = 0
                 local tick = 0
@@ -260,9 +266,8 @@ function InterceptionPoint(t,s,w,d,r)
                         end
                 end
                 if GetTickCount() < tick + max - max/7 then
-                        local dist = math.sqrt((myHero.pos.x - t.pos.x)*(myHero.pos.x - t.pos.x)+(myHero.pos.z - t.pos.z)*(myHero.pos.z - t.pos.z))
-                        if dist < r then
-                                return t.pos
+                        if distmt < r then
+                                return tpos
                         end
                 else
                         UtilsManager[id].IsImmobile = false
@@ -271,10 +276,9 @@ function InterceptionPoint(t,s,w,d,r)
         if UtilsManager[id].IsAttacking then
                 local lastaa = UtilsManager[id].AALast
                 local windup = UtilsManager[id].AACastDelay
-                local dist = math.sqrt((myHero.pos.x - t.pos.x)*(myHero.pos.x - t.pos.x)+(myHero.pos.z - t.pos.z)*(myHero.pos.z - t.pos.z))
                 if GetTickCount() < lastaa + windup - windup/7 then
-                        if dist < r then
-                                return t.pos
+                        if distmt < r then
+                                return tpos
                         end
                 else
                         UtilsManager[id].IsAttacking = false
@@ -292,17 +296,16 @@ function InterceptionPoint(t,s,w,d,r)
                         B.x = B.x + (dirx * GetMoveSpeed(t) * tAB)
                         B.z = B.z + (dirz * GetMoveSpeed(t) * tAB)
                 end
-                local dist = math.sqrt((myHero.pos.x - B.x)*(myHero.pos.x - B.x)+(myHero.pos.z - B.z)*(myHero.pos.z - B.z))
+                local dist = math.sqrt((mepos.x - B.x)*(mepos.x - B.x)+(mepos.z - B.z)*(mepos.z - B.z))
                 if GetTickCount() < WaypointManager[id].time + 100 or GetTickCount() > WaypointManager[id].time + 1000 then
                         if dist < r then
                                 return { x = B.x + (dirx * ( ( GetMoveSpeed(t) * d ) - ( w / 2) ) ), z = B.z + (dirz * ( ( GetMoveSpeed(t) * d ) - ( w / 2) ) ) }
                         end
                 end
         else
-                local dist = math.sqrt((myHero.pos.x - t.pos.x)*(myHero.pos.x - t.pos.x)+(myHero.pos.z - t.pos.z)*(myHero.pos.z - t.pos.z))
-                if dist < r then
+                if distmt < r then
                         if GetTickCount() < UtilsManager[id].LastStopMoveTime + 100 or dist < 300 then
-                                return t.pos
+                                return tpos
                         end
                 end
         end
