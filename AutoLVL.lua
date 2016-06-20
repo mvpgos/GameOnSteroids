f51a88652844ecdced92cfc60413c839d4bedfe6
gamsteron <gamsteron@gmail.com>
2016-06-20 20:44:22 +0200

    0.02

--- AutoLVL.lua
+++ AutoLVL.lua
@@ -1,4 +1,4 @@
-local ver = "0.01"
+local ver = "0.02"
 function AutoUpdate(data)
     if tonumber(data) > tonumber(ver) then
         DownloadFileAsync("https://raw.githubusercontent.com/gamsteron/GameOnSteroids/master/AutoLVL.lua", SCRIPT_PATH .. "AutoLVL.lua", function() PrintChat("Update Complete, please 2x F6!") return end)
@@ -8,107 +8,235 @@ function AutoUpdate(data)
 end
 GetWebResultAsync("https://raw.githubusercontent.com/gamsteron/GameOnSteroids/master/AutoLVL.version", AutoUpdate)
 
-Config = MenuConfig("GSOALU", "GamSterOn Auto LVL UP")
-Config:DropDown("SWITCH", "Level Up Mode ->", 2, {"CUSTOM", "RQWE", "RQEW", "RWQE", "RWEQ", "REQW", "REWQ"})
-Config:Slider("START", "Start Level", 2,1,2,1)
-Config:Menu("CUSTOM", "Custom selection")
-Config.CUSTOM:DropDown("FIRST", "1", 1, {"Q", "W", "E", "R"})
-Config.CUSTOM:DropDown("SECOND", "2", 1, {"Q", "W", "E", "R"})
-Config.CUSTOM:DropDown("THIRD", "3", 1, {"Q", "W", "E", "R"})
-Config.CUSTOM:DropDown("FOURTH", "4", 1, {"Q", "W", "E", "R"})
-Config.CUSTOM:DropDown("FIFTH", "5", 1, {"Q", "W", "E", "R"})
-Config.CUSTOM:DropDown("SIXTH", "6", 1, {"Q", "W", "E", "R"})
-Config.CUSTOM:DropDown("SEVENTH", "7", 1, {"Q", "W", "E", "R"})
-Config.CUSTOM:DropDown("EIGHTH", "8", 1, {"Q", "W", "E", "R"})
-Config.CUSTOM:DropDown("NINTH", "9", 1, {"Q", "W", "E", "R"})
-Config.CUSTOM:DropDown("TENTH", "10", 1, {"Q", "W", "E", "R"})
-Config.CUSTOM:DropDown("ELEVENTH", "11", 1, {"Q", "W", "E", "R"})
-Config.CUSTOM:DropDown("TWELFTH", "12", 1, {"Q", "W", "E", "R"})
-Config.CUSTOM:DropDown("THIRTEENTH", "13", 1, {"Q", "W", "E", "R"})
-Config.CUSTOM:DropDown("FOURTEENTH", "14", 1, {"Q", "W", "E", "R"})
-Config.CUSTOM:DropDown("FIFTEENTH", "15", 1, {"Q", "W", "E", "R"})
-Config.CUSTOM:DropDown("SIXTEENTH", "16", 1, {"Q", "W", "E", "R"})
-Config.CUSTOM:DropDown("SEVENTEENTH", "17", 1, {"Q", "W", "E", "R"})
-Config.CUSTOM:DropDown("EIGHTEENTH", "18", 1, {"Q", "W", "E", "R"})
+GSOALU = MenuConfig("gsoalu", "GamSterOn Auto LVL UP")
+GSOALU:DropDown("SWITCH", "Level Up Mode ->", 2, {"CUSTOM", "RQWE", "RQEW", "RWQE", "RWEQ", "REQW", "REWQ"})
+GSOALU:Boolean("HUMANIZER", "Humanizer", false)
+GSOALU:Slider("START", "Start Level", 2,1,2,1)
+GSOALU:Menu("CUSTOM", "Custom selection")
+GSOALU.CUSTOM:DropDown("FIRST", "1", 1, {"Q", "W", "E", "R"})
+GSOALU.CUSTOM:DropDown("SECOND", "2", 1, {"Q", "W", "E", "R"})
+GSOALU.CUSTOM:DropDown("THIRD", "3", 1, {"Q", "W", "E", "R"})
+GSOALU.CUSTOM:DropDown("FOURTH", "4", 1, {"Q", "W", "E", "R"})
+GSOALU.CUSTOM:DropDown("FIFTH", "5", 1, {"Q", "W", "E", "R"})
+GSOALU.CUSTOM:DropDown("SIXTH", "6", 1, {"Q", "W", "E", "R"})
+GSOALU.CUSTOM:DropDown("SEVENTH", "7", 1, {"Q", "W", "E", "R"})
+GSOALU.CUSTOM:DropDown("EIGHTH", "8", 1, {"Q", "W", "E", "R"})
+GSOALU.CUSTOM:DropDown("NINTH", "9", 1, {"Q", "W", "E", "R"})
+GSOALU.CUSTOM:DropDown("TENTH", "10", 1, {"Q", "W", "E", "R"})
+GSOALU.CUSTOM:DropDown("ELEVENTH", "11", 1, {"Q", "W", "E", "R"})
+GSOALU.CUSTOM:DropDown("TWELFTH", "12", 1, {"Q", "W", "E", "R"})
+GSOALU.CUSTOM:DropDown("THIRTEENTH", "13", 1, {"Q", "W", "E", "R"})
+GSOALU.CUSTOM:DropDown("FOURTEENTH", "14", 1, {"Q", "W", "E", "R"})
+GSOALU.CUSTOM:DropDown("FIFTEENTH", "15", 1, {"Q", "W", "E", "R"})
+GSOALU.CUSTOM:DropDown("SIXTEENTH", "16", 1, {"Q", "W", "E", "R"})
+GSOALU.CUSTOM:DropDown("SEVENTEENTH", "17", 1, {"Q", "W", "E", "R"})
+GSOALU.CUSTOM:DropDown("EIGHTEENTH", "18", 1, {"Q", "W", "E", "R"})
 
-local TIMER = 0
-local RQWE = {_Q,_W,_E,_Q,_Q,_R,_Q,_W,_Q,_W,_R,_W,_W,_E,_E,_R,_E,_E}
-local RQEW = {_Q,_E,_W,_Q,_Q,_R,_Q,_E,_Q,_E,_R,_E,_E,_W,_W,_R,_W,_W}
-local RWQE = {_W,_Q,_E,_W,_W,_R,_W,_Q,_W,_Q,_R,_Q,_Q,_E,_E,_R,_E,_E}
-local RWEQ = {_W,_E,_Q,_W,_W,_R,_W,_E,_W,_E,_R,_E,_E,_Q,_Q,_R,_Q,_Q}
-local REQW = {_E,_Q,_W,_E,_E,_R,_E,_Q,_E,_Q,_R,_Q,_Q,_W,_W,_R,_W,_W}
-local REWQ = {_E,_W,_Q,_E,_E,_R,_E,_W,_E,_W,_R,_W,_W,_Q,_Q,_R,_Q,_Q}
+local GSOALUTIMER = 0
+local GSOALURQWE = {_Q,_W,_E,_Q,_Q,_R,_Q,_W,_Q,_W,_R,_W,_W,_E,_E,_R,_E,_E}
+local GSOALURQEW = {_Q,_E,_W,_Q,_Q,_R,_Q,_E,_Q,_E,_R,_E,_E,_W,_W,_R,_W,_W}
+local GSOALURWQE = {_W,_Q,_E,_W,_W,_R,_W,_Q,_W,_Q,_R,_Q,_Q,_E,_E,_R,_E,_E}
+local GSOALURWEQ = {_W,_E,_Q,_W,_W,_R,_W,_E,_W,_E,_R,_E,_E,_Q,_Q,_R,_Q,_Q}
+local GSOALUREQW = {_E,_Q,_W,_E,_E,_R,_E,_Q,_E,_Q,_R,_Q,_Q,_W,_W,_R,_W,_W}
+local GSOALUREWQ = {_E,_W,_Q,_E,_E,_R,_E,_W,_E,_W,_R,_W,_W,_Q,_Q,_R,_Q,_Q}
 
 OnTick(function (myHero)
-        if GetTickCount() > TIMER + 1000 then
-                local CASE = Config.SWITCH:Value()
+        if GetTickCount() > GSOALUTIMER + 1000 then
+                local CASE = GSOALU.SWITCH:Value()
+                local START = GSOALU.START:Value()
                 local LEVEL = GetLevel(myHero)
                 local PLUS = GetCastLevel(myHero, _Q) + GetCastLevel(myHero, _W) + GetCastLevel(myHero, _E) + GetCastLevel(myHero, _R)
-                if LEVEL > PLUS and LEVEL >= Config.START:Value() then
+                if LEVEL > PLUS and LEVEL >= START then
                         if CASE == 1 then
                                 if LEVEL == 1 then
-                                        LevelCustomSpell(Config.CUSTOM.FIRST:Value())
+                                        GSOALULevelCustomSpell(GSOALU.CUSTOM.FIRST:Value())
                                 elseif LEVEL == 2 then
-                                        LevelCustomSpell(Config.CUSTOM.SECOND:Value())
+                                        GSOALULevelCustomSpell(GSOALU.CUSTOM.SECOND:Value())
                                 elseif LEVEL == 3 then
-                                        LevelCustomSpell(Config.CUSTOM.THIRD:Value())
+                                        GSOALULevelCustomSpell(GSOALU.CUSTOM.THIRD:Value())
                                 elseif LEVEL == 4 then
-                                        LevelCustomSpell(Config.CUSTOM.FOURTH:Value())
+                                        GSOALULevelCustomSpell(GSOALU.CUSTOM.FOURTH:Value())
                                 elseif LEVEL == 5 then
-                                        LevelCustomSpell(Config.CUSTOM.FIFTH:Value())
+                                        GSOALULevelCustomSpell(GSOALU.CUSTOM.FIFTH:Value())
                                 elseif LEVEL == 6 then
-                                        LevelCustomSpell(Config.CUSTOM.SIXTH:Value())
+                                        GSOALULevelCustomSpell(GSOALU.CUSTOM.SIXTH:Value())
                                 elseif LEVEL == 7 then
-                                        LevelCustomSpell(Config.CUSTOM.SEVENTH:Value())
+                                        GSOALULevelCustomSpell(GSOALU.CUSTOM.SEVENTH:Value())
                                 elseif LEVEL == 8 then
-                                        LevelCustomSpell(Config.CUSTOM.EIGHTH:Value())
+                                        GSOALULevelCustomSpell(GSOALU.CUSTOM.EIGHTH:Value())
                                 elseif LEVEL == 9 then
-                                        LevelCustomSpell(Config.CUSTOM.NINTH:Value())
+                                        GSOALULevelCustomSpell(GSOALU.CUSTOM.NINTH:Value())
                                 elseif LEVEL == 10 then
-                                        LevelCustomSpell(Config.CUSTOM.TENTH:Value())
+                                        GSOALULevelCustomSpell(GSOALU.CUSTOM.TENTH:Value())
                                 elseif LEVEL == 11 then
-                                        LevelCustomSpell(Config.CUSTOM.ELEVENTH:Value())
+                                        GSOALULevelCustomSpell(GSOALU.CUSTOM.ELEVENTH:Value())
                                 elseif LEVEL == 12 then
-                                        LevelCustomSpell(Config.CUSTOM.TWELFTH:Value())
+                                        GSOALULevelCustomSpell(GSOALU.CUSTOM.TWELFTH:Value())
                                 elseif LEVEL == 13 then
-                                        LevelCustomSpell(Config.CUSTOM.THIRTEENTH:Value())
+                                        GSOALULevelCustomSpell(GSOALU.CUSTOM.THIRTEENTH:Value())
                                 elseif LEVEL == 14 then
-                                        LevelCustomSpell(Config.CUSTOM.FOURTEENTH:Value())
+                                        GSOALULevelCustomSpell(GSOALU.CUSTOM.FOURTEENTH:Value())
                                 elseif LEVEL == 15 then
-                                        LevelCustomSpell(Config.CUSTOM.FIFTEENTH:Value())
+                                        GSOALULevelCustomSpell(GSOALU.CUSTOM.FIFTEENTH:Value())
                                 elseif LEVEL == 16 then
-                                        LevelCustomSpell(Config.CUSTOM.SIXTEENTH:Value())
+                                        GSOALULevelCustomSpell(GSOALU.CUSTOM.SIXTEENTH:Value())
                                 elseif LEVEL == 17 then
-                                        LevelCustomSpell(Config.CUSTOM.SEVENTEENTH:Value())
+                                        GSOALULevelCustomSpell(GSOALU.CUSTOM.SEVENTEENTH:Value())
                                 else
-                                        LevelCustomSpell(Config.CUSTOM.EIGHTEENTH:Value())
+                                        GSOALULevelCustomSpell(GSOALU.CUSTOM.EIGHTEENTH:Value())
                                 end
                         elseif CASE == 2 then
-                                LevelSpell(RQWE[LEVEL])
+                                GSOALULevelUp(LEVEL, 1, _W, GSOALURQWE[LEVEL])
                         elseif CASE == 3 then
-                                LevelSpell(RQEW[LEVEL])
+                                GSOALULevelUp(LEVEL, 1, _E, GSOALURQEW[LEVEL])
                         elseif CASE == 4 then
-                                LevelSpell(RWQE[LEVEL])
+                                GSOALULevelUp(LEVEL, 2, _Q, GSOALURWQE[LEVEL])
                         elseif CASE == 5 then
-                                LevelSpell(RWEQ[LEVEL])
+                                GSOALULevelUp(LEVEL, 2, _E, GSOALURWEQ[LEVEL])
                         elseif CASE == 6 then
-                                LevelSpell(REQW[LEVEL])
+                                GSOALULevelUp(LEVEL, 3, _Q, GSOALUREQW[LEVEL])
                         else
-                                LevelSpell(REWQ[LEVEL])
+                                GSOALULevelUp(LEVEL, 3, _W, GSOALUREWQ[LEVEL])
                         end
                 end
-                TIMER = GetTickCount()
+                GSOALUTIMER = GetTickCount()
         end
 end)
 
-function LevelCustomSpell(num)
-        if num == 1 then
-                LevelSpell(_Q)
-        elseif num == 2 then
-                LevelSpell(_W)
-        elseif num == 3 then
-                LevelSpell(_E)
+function GSOALULevelUp(level, dolvlfirst, dolvlsecond, dolvlfromtable)
+        local num = GenerateRandomFloat(0.5, 1)
+        if level == 2 then
+                if dolvlfirst == 1 then 
+                        local q = GetCastLevel(myHero, _Q)
+                        if q == 1 then
+                                if GSOALU.HUMANIZER:Value() then
+                                        DelayAction(function() LevelSpell(dolvlsecond) end,num)
+                                else
+                                        LevelSpell(dolvlsecond)
+                                end
+                        else
+                                if GSOALU.HUMANIZER:Value() then
+                                        DelayAction(function() LevelSpell(_Q) end,num)
+                                else
+                                        LevelSpell(_Q)
+                                end
+                        end
+                elseif dolvlfirst == 2 then
+                        local w = GetCastLevel(myHero, _W)
+                        if w == 1 then
+                                if GSOALU.HUMANIZER:Value() then
+                                        DelayAction(function() LevelSpell(dolvlsecond) end,num)
+                                else
+                                        LevelSpell(dolvlsecond)
+                                end
+                        else
+                                if GSOALU.HUMANIZER:Value() then
+                                        DelayAction(function() LevelSpell(_W) end,num)
+                                else
+                                        LevelSpell(_W)
+                                end
+                        end
+                else
+                        local e = GetCastLevel(myHero, _E)
+                        if e == 1 then
+                                if GSOALU.HUMANIZER:Value() then
+                                        DelayAction(function() LevelSpell(dolvlsecond) end,num)
+                                else
+                                        LevelSpell(dolvlsecond)
+                                end
+                        else
+                                if GSOALU.HUMANIZER:Value() then
+                                        DelayAction(function() LevelSpell(_E) end,num)
+                                else
+                                        LevelSpell(_E)
+                                end
+                        end
+                end
+        elseif level == 3 then
+                if dolvlfirst == 1 then
+                        local w = GetCastLevel(myHero, _W)
+                        if w == 1 then
+                                if GSOALU.HUMANIZER:Value() then
+                                        DelayAction(function() LevelSpell(_E) end,num)
+                                else
+                                        LevelSpell(_E)
+                                end
+                        else
+                                if GSOALU.HUMANIZER:Value() then
+                                        DelayAction(function() LevelSpell(_W) end,num)
+                                end
+                                        LevelSpell(_W)
+                                end
+                        end
+                elseif dolvlfirst == 2 then
+                        local q = GetCastLevel(myHero, _Q)
+                        if q == 1 then
+                                if GSOALU.HUMANIZER:Value() then
+                                        DelayAction(function() LevelSpell(_E) end,num)
+                                else
+                                        LevelSpell(_E)
+                                end
+                        else
+                                if GSOALU.HUMANIZER:Value() then
+                                        DelayAction(function() LevelSpell(_Q) end,num)
+                                else
+                                        LevelSpell(_Q)
+                                end
+                        end
+                else
+                        local q = GetCastLevel(myHero, _Q)
+                        if q == 1 then
+                                if GSOALU.HUMANIZER:Value() then
+                                        DelayAction(function() LevelSpell(_W) end,num)
+                                else
+                                        LevelSpell(_W)
+                                end
+                        else
+                                if GSOALU.HUMANIZER:Value() then
+                                        DelayAction(function() LevelSpell(_Q) end,num)
+                                else
+                                        LevelSpell(_Q)
+                                end
+                        end
+                end
         else
-                LevelSpell(_R)
+                if GSOALU.HUMANIZER:Value() then
+                        DelayAction(function() LevelSpell(dolvlfromtable) end,num)
+                else
+                        LevelSpell(dolvlfromtable)
+                end
         end
+end
+
+function GSOALULevelCustomSpell(spell)
+        local num = GenerateRandomFloat(0.5, 1)
+        if spell == 1 then
+                if GSOALU.HUMANIZER:Value() then
+                        DelayAction(function() LevelSpell(_Q) end,num)
+                else
+                        LevelSpell(_Q)
+                end
+        elseif spell == 2 then
+                if GSOALU.HUMANIZER:Value() then
+                        DelayAction(function() LevelSpell(_W) end,num)
+                else
+                        LevelSpell(_W)
+                end
+        elseif spell == 3 then
+                if GSOALU.HUMANIZER:Value() then
+                        DelayAction(function() LevelSpell(_E) end,num)
+                else
+                        LevelSpell(_E)
+                end
+        else
+                if GSOALU.HUMANIZER:Value() then
+                        DelayAction(function() LevelSpell(_R) end,num)
+                else
+                        LevelSpell(_R)
+                end
+        end
+end
+
+function GenerateRandomFloat(lower, greater)
+    return lower + math.random()  * (greater - lower)
 end
